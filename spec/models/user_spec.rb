3262f64c468551b4747c1e22e954b042e8561fac
mumbereh <siirasonh@gmail.com>
2023-11-04 23:43:13 +0300

    Fixed tests errors as required by the reviewer

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,16 +18,33 @@ RSpec.describe User, type: :model do
     end
   end
   describe '#recent_posts' do
-    it 'returns the 3 most recent posts' do
-      user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
-      post1 = Post.create(author: user, title: 'Post 1', text: 'hello world', comments_counter: 0,
-                          likes_counter: 0, created_at: 2.days.ago)
-      post2 = Post.create(author: user, title: 'Post 2', text: 'hello world', comments_counter: 0,
-                          likes_counter: 0, created_at: 2.days.ago)
-      post3 = Post.create(author: user, title: 'Post 3', text: 'hello world', comments_counter: 0,
-                          likes_counter: 0, created_at: 2.days.ago)
-      rec_posts = user.recent_posts
-      expect(rec_posts).to eq([post3, post2, post1])
+    it 'should display the 3 last recents posts' do
+      author_object = User.new(name: 'Ariel_CEO')
+      author_object.posts_counter = 0
+      author_object.save
+      user = User.new(name: 'Ariel_CEO')
+      user.posts_counter = 0
+      user.save
+      post1 = Post.new(title: 'Microverse School')
+      post1.author_id = user.id
+      post1.text = 'I love Microverse'
+      post1.comments_counter = 0
+      post1.likes_counter = 0
+      post1.save
+      post2 = Post.new(title: 'Microverse School')
+      post2.author_id = user.id
+      post2.text = 'I love Microverse'
+      post2.comments_counter = 0
+      post2.likes_counter = 0
+      post2.save
+      post3 = Post.new(title: 'Microverse School')
+      post3.author_id = user.id
+      post3.text = 'I love Microverse'
+      post3.comments_counter = 0
+      post3.likes_counter = 0
+      post3.save
+      posts_number = user.user_recent_posts
+      expect(posts_number.count).to eq(3)
     end
   end
 end
