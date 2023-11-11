9abb095ad94b9dddfda6b9ebfef044b14053fd5b
mumbereh <siirasonh@gmail.com>
2023-11-12 02:00:20 +0300

    Add creat function to Update config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,18 @@
 Rails.application.routes.draw do
   root "users#index"
 
-
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 
-  resources :posts do
+  resources :posts, only: [:index, :show, :new, :create] do
     resources :comments, only: [:new, :create]
-
-  
     resources :likes, only: [:new, :create]
-
 
     member do
       post 'like'
     end
   end
-
 
   get '/posts/:post_id/comments/new', to: 'comments#new', as: 'new_post_comment'
 end
