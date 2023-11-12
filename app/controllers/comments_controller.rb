0d7e740e356ec8854e091d14b327d538b9028e48
mumbereh <siirasonh@gmail.com>
2023-11-12 09:57:01 +0300

    Add creat function to Update route.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,19 @@
 class CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
+    @comment = Comment.new(comment_params)
     @comment.user = current_user
-    @comment.save
-    redirect_to post_path(@post)
+    @post = Post.find(params[:post_id])
+
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Comment was successfully created.'
+    else
+      render 'new'
+    end
   end
 
   def new
     @post = Post.find(params[:post_id])
-    @comment = current_user.comment.build
+    @comment = current_user.comments.build
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-  Rails.application.routes.draw do
-    root "users#index"
-    resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :show, :new, :create] do
-        resources :comments ,only:[:index,:new,:create]
-        resources :likes ,only:[:create]
-      end
+Rails.application.routes.draw do
+  root "users#index"
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
     end
   end
-  
+end
