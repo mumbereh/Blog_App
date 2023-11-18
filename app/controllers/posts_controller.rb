fd32c0e071336b1835d3a64aef838e4dda41303f
mumbereh <siirasonh@gmail.com>
2023-11-18 04:42:29 +0300

    configured linter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,47 +1,9 @@
 class PostsController < ApplicationController
   layout 'standard'
 
-
-  def index
-    @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id]).includes(:comments).order(id: :asc)
-    @posts = @posts.paginate(page: params[:page], per_page: 5)
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(params.require(:post).permit(:title, :text))
-    @post.author = current_user
-    if @post.save
-      flash[:success] = 'Post created successfully!'
-      redirect_to user_posts_url
-    else
-      flash.now[:error] = 'Error: Post could not be created!'
-      render :new, locals: { post: @post }
-    end
-  end
-end
-<<<<<<< HEAD
-=======
-class PostsController < ApplicationController
-  layout 'standard'
-
-  def index
-    @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id]).includes(:comments).order(id: :asc)
-    @posts = @posts.paginate(page: params[:page], per_page: 5)
-
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments).order(id: :asc).paginate(page: params[:page], per_page: 5)
-
   end
 
   def show
@@ -65,10 +27,33 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:success] = 'Post updated successfully!'
+      redirect_to user_post_url(current_user, @post)
+    else
+      flash.now[:error] = 'Error: Post could not be updated!'
+      render :edit, locals: { post: @post }
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    flash[:success] = 'Post deleted successfully!'
+    redirect_to user_posts_url(current_user)
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :text)
   end
 end
->>>>>>> 7d2e1b249f324e6c25b9c0e59838e17b9a5dbcc4
