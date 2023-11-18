99e63e2e14791d40bcbe4c89ac9c70e75d2da22c
mumbereh <siirasonh@gmail.com>
2023-11-18 04:29:02 +0300

    Add devise to the project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,32 +27,3 @@ class PostsController < ApplicationController
     end
   end
 end
-class PostsController < ApplicationController
-  layout 'standard'
-
-  def index
-    @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id]).includes(:comments).order(id: :asc)
-    @posts = @posts.paginate(page: params[:page], per_page: 5)
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(params.require(:post).permit(:title, :text))
-    @post.author = current_user
-    if @post.save
-      flash[:success] = 'Post created successfully!'
-      redirect_to user_posts_url
-    else
-      flash.now[:error] = 'Error: Post could not be created!'
-      render :new, locals: { post: @post }
-    end
-  end
-end
