2c586f34fe73552b87a8644ca10eeae1dba29852
mumbereh <siirasonh@gmail.com>
2023-11-18 16:45:30 +0300

    fixed display/route error

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-    root "users#index"
-  
-    resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :new, :show, :create] do
-        resources :comments, only: [:new, :create]
-        resources :likes, only: [:create]
-      end
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :new, :show, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
     end
   end
+end
